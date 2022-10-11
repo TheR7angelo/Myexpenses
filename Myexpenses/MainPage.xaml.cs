970a5edc10f4713280227c93da575f9b348b3d58
TheR7angelo <raphael.antunes@homtail.fr>
2022-10-11 22:36:26 +0200

    test camera

--- Myexpenses/MainPage.xaml.cs
+++ Myexpenses/MainPage.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
 using MyExpenses.Utils;
+using Xamarin.Essentials;
 
 namespace MyExpenses
 {
@@ -13,8 +16,46 @@ namespace MyExpenses
         private void Button_OnClicked(object sender, EventArgs e)
         {
             LabelLocation.Text = Permission.Camera() ? "gagner" : "perdu";
+            var takePhotoAsync = TakePhotoAsync();
+            LabelLocation.Text = takePhotoAsync.ToString();
         }
-        
-        
+
+        private async Task TakePhotoAsync()
+        {
+            try
+            {
+                var photo = await MediaPicker.CapturePhotoAsync();
+                await LoadPhotoAsync(photo);
+                //Console.WriteLine($"CapturePhotoAsync COMPLETED: {PhotoPath}");
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Feature is not supported on the device
+            }
+            catch (PermissionException pEx)
+            {
+                // Permissions not granted
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"CapturePhotoAsync THREW: {ex.Message}");
+            }
+        }
+
+        private async Task LoadPhotoAsync(FileResult photo)
+        {
+            // canceled
+            if (photo == null)
+            {
+                return;
+            }
+
+            // save the file into local storage
+            var newFile = Path.Combine(FileSystem.CacheDirectory, photo.FileName);
+            using (var stream = await photo.OpenReadAsync())
+            using (var newStream = File.OpenWrite(newFile))
+                await stream.CopyToAsync(newStream);
+        }
+
     }
 }
