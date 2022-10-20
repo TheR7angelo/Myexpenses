cdff6761c9db4234db8d15313c6d2c84e01fd2c5
TheR7angelo <raphael.antunes@hotmail.fr>
2022-10-20 09:36:08 +0200

    up

--- MyExpenses/Utils/DataBase/Request.cs
+++ MyExpenses/Utils/DataBase/Request.cs
@@ -6,6 +6,9 @@ public static partial class SqLite
 {
     public static List<TAccountClass> GetAllAccount() => _connection!.QueryAsync<TAccountClass>("SELECT * FROM t_compte").Result;
 
+    public static List<TColorsClass> GetAllColor() =>
+        _connection!.QueryAsync<TColorsClass>("SELECT * FROM t_colors").Result;
+
     public static List<VTotalByAccountClass> GetVTotalByAccountClass() => _connection!
         .QueryAsync<VTotalByAccountClass>(
             "SELECT h.compte, round(sum(h.montant), 2) as restant  FROM v_historique h GROUP BY h.compte;").Result;
--- MyExpenses/Views/AddWallet.xaml.cs
+++ MyExpenses/Views/AddWallet.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using MyExpenses.Utils.Database;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace MyExpenses.Views;
@@ -9,9 +12,26 @@ public partial class AddWallet
     public AddWallet()
     {
         InitializeComponent();
+
+        FillComboColor();
         //todo finir les inputs et récupérer les données
     }
 
+    private void FillComboColor()
+    {
+        var colors = SqLite.GetAllColor();
+        var i = new List<string>();
+        foreach (var color in colors)
+        {
+            
+            // var la = new Label { BackgroundColor = Color.FromHex(color.Value), Text = color.Nom};
+            i.Add(color.Nom);
+        }
+
+        ComboBoxColor.ItemsSource = i;
+        // todo à finir marche po cette merde
+    }
+    
     private void Button_OnClicked(object sender, EventArgs e)
     {
         throw new NotImplementedException();
