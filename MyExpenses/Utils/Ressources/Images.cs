afb9cc91022eaea7f7bfcc4f9e8c4d872ce3130b
TheR7angelo <raphael.antunes@hotmail.fr>
2022-10-22 22:30:26 +0200

    add skPath

--- MyExpenses/Utils/Ressources/Images.cs
+++ MyExpenses/Utils/Ressources/Images.cs
@@ -1,25 +1,59 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using MyExpenses.Utils.Ressources.Style;
+using SkiaSharp.Views.Forms;
+using Xamarin.Forms;
+using Xamarin.Forms.Shapes;
 
 namespace MyExpenses.Utils.Ressources;
 
 public static class Images
 {
-    private static readonly List<Structs.Images> List = new ();
+    private static readonly List<Structs.Paths> List;
 
     static Images()
     {
-        
+        List = new List<Structs.Paths>
+        {
+            Money()
+        };
     }
+    
+    public static List<Structs.Paths> GetAllImages() => List;
 
-
-    public static List<Structs.Images> GetAllImages() => List;
-
-    public static Xamarin.Forms.ImageSource GetImageSource(this string name)
+    public static Path GetImage(this string name)
     {
         var st = List.Where(s => s.Name.Equals(name)).ToList();
-        return st.Count.Equals(0) ? null : st[0].VImageSource;
+        return st.Count.Equals(0) ? null : st[0].VPath;
+    }
+
+    private static Structs.Paths Money()
+    {
+        var path = new Path();
+        var group = new GeometryGroup();
+
+
+        var i = _geometry(
+            "M430.545,232.727c-26.764,0-51.2,12.8-65.164,33.745C353.745,260.655,340.945,256,325.818,256\n\tc-40.727,0-74.473,30.255-80.291,69.818h243.2c11.636,0,20.945-8.145,23.273-19.782\n\tC507.345,265.309,472.436,232.727,430.545,232.727z");
+
+        group.Children.Add(i);
+
+
+
+
+
+        path.Data = group;
+        return new Structs.Paths()
+        {
+            Name = "Money",
+            VPath = path
+        };
+    }
+
+    private static Geometry _geometry(this string geom)
+    {
+        var path = new PathGeometryConverter();
+        return (Geometry)path.ConvertFromInvariantString(geom);
     }
 
 }
--- MyExpenses/Views/AddWallet.xaml.cs
+++ MyExpenses/Views/AddWallet.xaml.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using MyExpenses.Utils.Database;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
 using Xamarin.Forms;
+using Xamarin.Forms.Shapes;
 using Xamarin.Forms.Xaml;
 
 namespace MyExpenses.Views;
@@ -139,7 +142,30 @@ public partial class AddWallet
     {
         var combo = sender as Picker;
         var imageName = combo!.SelectedItem as string;
-        var image = Utils.Ressources.Images.GetImage(imageName);
-        PathImage.Data = image.Data;
+        // var image = Utils.Ressources.Images.GetImage(imageName);
+        
+
+        // PathImage.Data = image.Data;
+    }
+
+    private void SkCanvasView_OnPaintSurface(object sender, SKPaintSurfaceEventArgs e)
+    {
+        var info = e.Info;
+        var surface = e.Surface;
+        var canvas = surface.Canvas;
+
+        // todo a fini xD
+        canvas.Clear();
+        // var path = SKPath.ParseSvgPathData("M430.545,232.727c-26.764,0-51.2,12.8-65.164,33.745C353.745,260.655,340.945,256,325.818,256\n\tc-40.727,0-74.473,30.255-80.291,69.818h243.2c11.636,0,20.945-8.145,23.273-19.782\n\tC507.345,265.309,472.436,232.727,430.545,232.727z");
+        var pathStyle = new SKPaint()
+        {
+            Style = SKPaintStyle.Stroke,
+            Color = SKColors.Brown,
+            StrokeWidth = 10
+        };
+        
+        //https://stackoverflow.com/questions/50288603/dynamically-draw-lines-with-skiasharp-in-xamarin
+        
+        canvas.DrawLine(0, 0, 2, 50, pathStyle);
     }
 }
