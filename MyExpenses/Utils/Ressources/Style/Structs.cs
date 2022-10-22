e0bad69004da46043c749268da60eec330e6371f
TheR7angelo <raphael.antunes@hotmail.fr>
2022-10-22 12:07:32 +0200

    added new struc

--- MyExpenses/Utils/Ressources/Style/Structs.cs
+++ MyExpenses/Utils/Ressources/Style/Structs.cs
@@ -1,10 +1,19 @@
-﻿namespace MyExpenses.Utils.Ressources.Style;
+﻿using Xamarin.Forms;
 
-public partial class Structs
+namespace MyExpenses.Utils.Ressources.Style;
+
+public static partial class Structs
 {
     public struct Style
     {
         public string Name;
         public Xamarin.Forms.Style VStyle;
+    }
+    
+    
+    public struct Images
+    {
+        public string Name;
+        public ImageSource VImageSource;
     }
 }
