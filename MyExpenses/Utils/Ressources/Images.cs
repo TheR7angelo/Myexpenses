2ad7b6fca8ed6217f8e64d6028c357d25f9a2e90
TheR7angelo <raphael.antunes@hotmail.fr>
2022-10-23 14:33:22 +0200

    up

--- MyExpenses/Utils/Ressources/Images.cs
+++ MyExpenses/Utils/Ressources/Images.cs
@@ -32,16 +32,18 @@ public static class Images
     {
         var bitmap = new SKBitmap(512, 512);
         var canvas = new SKCanvas(bitmap);
-        var pathStyle = new SKPaint
+        //todo la path se génére mal
+        const string str1 = "M488.727,325.818H23.273C10.473,325.818,0,315.345,0,302.545V-256C0-12.8,10.473-23.273,23.273-23.273h465.455c12.8,0,23.273,10.473,23.273,23.273v256C512,315.345,501.527,325.818,488.727,325.818z";
+        var p1 = SKPath.ParseSvgPathData(str1.ToUpper());
+        var s1 = new SKPaint
         {
-            Style = SKPaintStyle.Stroke,
-            Color = SKColors.Brown,
-            StrokeWidth = 10
+            Style = SKPaintStyle.StrokeAndFill,
+            Color = SKColor.Parse("#FF0E9347"),
         };
-        canvas.DrawLine(0, 0, 2, 50, pathStyle);
+        // canvas.DrawLine(0, 0, 2, 50, pathStyle);
 
-        
-        
+        canvas.DrawPath(p1, s1);
+
         // var i = _geometry(
         //     "M430.545,232.727c-26.764,0-51.2,12.8-65.164,33.745C353.745,260.655,340.945,256,325.818,256\n\tc-40.727,0-74.473,30.255-80.291,69.818h243.2c11.636,0,20.945-8.145,23.273-19.782\n\tC507.345,265.309,472.436,232.727,430.545,232.727z");
         return new Structs.Canvas()
--- MyExpenses/Views/AddWallet.xaml.cs
+++ MyExpenses/Views/AddWallet.xaml.cs
@@ -6,7 +6,6 @@ using MyExpenses.Utils.Database;
 using SkiaSharp;
 using SkiaSharp.Views.Forms;
 using Xamarin.Forms;
-using Xamarin.Forms.Shapes;
 using Xamarin.Forms.Xaml;
 
 namespace MyExpenses.Views;
@@ -140,12 +139,10 @@ public partial class AddWallet
 
     private void PickerImage_OnSelectedIndexChanged(object sender, EventArgs e)
     {
-        var combo = sender as Picker;
-        var imageName = combo!.SelectedItem as string;
-        var image = Utils.Ressources.Images.GetImage(imageName);
-        
-
-        // PathImage.Data = image.Data;
+        // var combo = sender as Picker;
+        // var imageName = combo!.SelectedItem as string;
+        // var image = Utils.Ressources.Images.GetImage(imageName);
+        SkCanvasView.InvalidateSurface();
     }
 
     private void SkCanvasView_OnPaintSurface(object sender, SKPaintSurfaceEventArgs e)
@@ -159,17 +156,8 @@ public partial class AddWallet
 
         // todo a fini xD
         canvas.Clear();
-        // // var path = SKPath.ParseSvgPathData("M430.545,232.727c-26.764,0-51.2,12.8-65.164,33.745C353.745,260.655,340.945,256,325.818,256\n\tc-40.727,0-74.473,30.255-80.291,69.818h243.2c11.636,0,20.945-8.145,23.273-19.782\n\tC507.345,265.309,472.436,232.727,430.545,232.727z");
-        var pathStyle = new SKPaint()
-        {
-            Style = SKPaintStyle.Stroke,
-            Color = SKColors.Brown,
-            StrokeWidth = 10
-        };
-        //
-        // //https://stackoverflow.com/questions/50288603/dynamically-draw-lines-with-skiasharp-in-xamarin
-        //
-        // canvas.DrawLine(0, 0, 2, 50, pathStyle);
-        canvas.DrawBitmap(image, 0, 0);
+        var make = image.Resize(info, SKFilterQuality.High);
+
+        canvas.DrawBitmap(make, 0, 0);
     }
 }
