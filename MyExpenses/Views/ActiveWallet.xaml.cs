fe82b1985b0fc5ca0e61547615f931b6440726ba
TheR7angelo <raphael.antunes@hotmail.fr>
2022-10-28 14:53:22 +0200

    add new page

---
+++ MyExpenses/Views/ActiveWallet.xaml.cs
@@ -0,0 +1,14 @@
+﻿using MyExpenses.Utils.Database;
+using Xamarin.Forms.Xaml;
+
+namespace MyExpenses.Views;
+
+[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class ActiveWallet
+{
+    public ActiveWallet(SqLite.VWalletClass wallet)
+    {
+        InitializeComponent();
+        
+    }
+}
