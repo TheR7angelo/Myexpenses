d0dacf38b23822ee3b42ad1c597ad2417ca114d4
TheR7angelo <raphael.antunes@hotmail.fr>
2022-10-29 13:22:55 +0200

    add new page

---
+++ MyExpenses/Views/DisplayMagasin.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using MyExpenses.Utils.Database;
+using Xamarin.Forms.Xaml;
+
+namespace MyExpenses.Views;
+
+[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class DisplayMagasin
+{
+    private static List<SqLite.TLieuClass> _dataStore;
+    public DisplayMagasin()
+    {
+        InitializeComponent();
+        _dataStore = SqLite.GetAllLieu();
+    }
+}
