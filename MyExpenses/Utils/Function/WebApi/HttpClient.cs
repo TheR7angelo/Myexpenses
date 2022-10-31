0070e1c779627a8e07adee041fca0f2c0801208a
TheR7angelo <raphael.antunes@hotmail.fr>
2022-10-31 11:15:01 +0100

    add http client

---
+++ MyExpenses/Utils/Function/WebApi/HttpClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Http;
+
+namespace MyExpenses.Utils.Function.WebApi;
+
+public abstract class Http
+{
+    public static HttpClient GetHttpClient(string userAgent, string baseUrl)
+    {
+        var httpClient = new HttpClient { BaseAddress = new Uri(baseUrl)};
+        httpClient.DefaultRequestHeaders.Add("User-Agent", userAgent);
+        return httpClient;
+    }
+    
+    public static string ParseToUrlFormat(string str) => str.Replace(" ", "+");
+}
