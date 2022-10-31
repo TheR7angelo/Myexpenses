b8aca0bb6520832a0d15c8d753d034cf7ed034e8
TheR7angelo <raphael.antunes@hotmail.fr>
2022-10-31 11:15:16 +0100

    add test api nominatim

---
+++ MyExpenses/Utils/Function/WebApi/Nominatim/Nominatim.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Mapsui.UI.Forms;
+using Newtonsoft.Json;
+
+namespace MyExpenses.Utils.Function.WebApi;
+
+public partial class Nominatim : Http
+{
+    //todo application name
+    private static HttpClient HttpClient { get; } = GetHttpClient("C#", "https://nominatim.openstreetmap.org");
+    
+    public static Position AddressToPoint(string address) => _AddressToPosition(address).Result;
+    public static string? PointToAddress(Position position) => _PositionToAddress(position).Result;
+    private static async Task<Position> _AddressToPosition(string address)
+    {
+        var latitude = 0f;
+        var longitude = 0f;
+        try
+        {
+            var httpResult = await HttpClient.GetAsync($"search?q={ParseToUrlFormat(address)}&format=json&polygon=1&addressdetails=1").ConfigureAwait(false);
+            var result = await httpResult.Content.ReadAsStringAsync();
+        
+            var r = JsonConvert.DeserializeObject<List<NominatimStruc>>(result);
+            latitude = r![0].lat;
+            longitude = r[0].lon;
+        }
+        catch (Exception)
+        {
+            // ignored
+        }
+        return new Position (latitude, longitude);
+    }
+    
+    private static async Task<string?> _PositionToAddress(Position position)
+    {
+        var r = new NominatimStruc();
+        try
+        {
+            var httpResult = await HttpClient.GetAsync($"reverse?format=json&lat={position.Latitude.ToString(CultureInfo.InvariantCulture)}&lon={position.Longitude.ToString(CultureInfo.InvariantCulture)}").ConfigureAwait(false);
+            var result = await httpResult.Content.ReadAsStringAsync();
+            r = JsonConvert.DeserializeObject<NominatimStruc>(result);
+        }
+        catch (Exception)
+        {
+            // ignored
+        }
+
+        return r.display_name;
+    }
+}
---
+++ MyExpenses/Utils/Function/WebApi/Nominatim/NominatimStruc.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyExpenses.Utils.Function.WebApi;
+
+public partial class Nominatim
+{
+    private struct NominatimStruc
+    {
+        public long place_id = 0;
+        public string? licence = null;
+        public string? osm_type = null;
+        public long osm_id = 0;
+        public float lat = 0;
+        public float lon = 0;
+        public string? display_name = null;
+        public Address address = new ();
+        public IEnumerable<float> boundingbox = Array.Empty<float>();
+
+        public NominatimStruc()
+        {
+        }
+    }
+    
+    private struct Address
+    {
+        public string? house_number = null;
+        public string? road = null;
+        public string? suburb = null;
+        public string? city = null;
+        public string? municipality = null;
+        public string? county = null;
+        public string? state = null;
+        public string? region = null;
+        public long postcode = 0;
+        public string? country = null;
+        public string? country_code = null;
+
+        public Address()
+        {
+        }
+    }
+}
