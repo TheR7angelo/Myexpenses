8393ce7ca733e351c369ce85e1d2d0754761bf3b
TheR7angelo <raphael.antunes@hotmail.fr>
2022-11-02 13:05:28 +0100

    rename function

--- MyExpenses/Utils/DataBase/Insert.cs
+++ MyExpenses/Utils/DataBase/Insert.cs
@@ -2,25 +2,25 @@
 
 public static partial class SqLite
 {
-    public static HistoriqueClass InsertHistorique(this HistoriqueClass historiqueClass)
+    public static HistoriqueClass Insert(this HistoriqueClass historiqueClass)
     {
         _connection!.InsertAsync(historiqueClass).Wait();
         return historiqueClass;
     }
 
-    public static LieuClass InsertLieu(this LieuClass lieuClass)
+    public static LieuClass Insert(this LieuClass lieuClass)
     {
         _connection!.InsertAsync(lieuClass).Wait();
         return lieuClass;
     }
     
-    public static WalletClass InsertWallet(this WalletClass walletClass)
+    public static WalletClass Insert(this WalletClass walletClass)
     {
         _connection!.InsertAsync(walletClass).Wait();
         return walletClass;
     }
     
-    public static WalletType InsertWalletType(this WalletType walletType)
+    public static WalletType Insert(this WalletType walletType)
     {
         _connection!.InsertAsync(walletType).Wait();
         return walletType;
