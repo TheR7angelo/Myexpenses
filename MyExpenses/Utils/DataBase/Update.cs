b666f2910b4f10003a5bdfb17ce46308cab01044
TheR7angelo <raphael.antunes@hotmail.fr>
2022-11-06 00:01:02 +0100

    add new function and clean

--- MyExpenses/Utils/DataBase/Update.cs
+++ MyExpenses/Utils/DataBase/Update.cs
@@ -2,7 +2,13 @@
 
 public static partial class SqLite
 {
-    public static LieuClass UpdateLieuClass(this LieuClass lieuClass)
+    public static CategoryClass Update(this CategoryClass categoryClass)
+    {
+        _connection!.UpdateAsync(categoryClass).Wait();
+        return categoryClass;
+    }
+    
+    public static LieuClass Update(this LieuClass lieuClass)
     {
         _connection!.UpdateAsync(lieuClass).Wait();
         return lieuClass;
