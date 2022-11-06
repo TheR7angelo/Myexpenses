d1a889a4bdd8594ee1f2bcd0b97b872810632e85
TheR7angelo <raphael.antunes@hotmail.fr>
2022-11-06 19:04:30 +0100

    clean

--- MyExpenses/Utils/DataBase/Insert.cs
+++ MyExpenses/Utils/DataBase/Insert.cs
@@ -4,12 +4,6 @@ namespace MyExpenses.Utils.Database;
 
 public static partial class SqLite
 {
-    // public static CategoryClass Insert(this CategoryClass categoryClass)
-    // {
-    //     _connection!.InsertAsync(categoryClass).Wait();
-    //     return categoryClass;
-    // }
-    
     public static HistoriqueClass Insert(this HistoriqueClass historiqueClass)
     {
         _connection!.InsertAsync(historiqueClass).Wait();
--- MyExpenses/Utils/DataBase/Update.cs
+++ MyExpenses/Utils/DataBase/Update.cs
@@ -4,12 +4,6 @@ namespace MyExpenses.Utils.Database;
 
 public static partial class SqLite
 {
-    public static CategoryClass Update(this CategoryClass categoryClass)
-    {
-        _connection!.UpdateAsync(categoryClass).Wait();
-        return categoryClass;
-    }
-    
     public static LieuClass Update(this LieuClass lieuClass)
     {
         _connection!.UpdateAsync(lieuClass).Wait();
