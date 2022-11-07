3307e0099151e335d33f0ac0c25ce05f92fc8d9f
TheR7angelo <raphael.antunes@hotmail.fr>
2022-11-07 09:29:32 +0100

    add new test

--- MyExpenses/Views/DisplaySubscription.xaml.cs
+++ MyExpenses/Views/DisplaySubscription.xaml.cs
@@ -1,5 +1,7 @@
-﻿using MyExpenses.Utils.Database;
-using Xamarin.Forms;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using MyExpenses.Utils.Database;
 using Xamarin.Forms.Xaml;
 
 namespace MyExpenses.Views;
@@ -7,11 +9,48 @@ namespace MyExpenses.Views;
 [XamlCompilation(XamlCompilationOptions.Compile)]
 public partial class DisplaySubscription
 {
+    private static readonly ObservableCollection<SqLite.SubscriptionClass> Subscription = new ();
     public DisplaySubscription()
     {
-        InitializeComponent();
+        try
+        {
+            InitializeComponent();
+            DataGrid.ItemsSource = Subscription;
 
-        var data = SqLite.GetAllSubscription();
-        DataGrid.ItemsSource = data;
+            SqLite.GetAllSubscription().ForEach(s => Subscription.Add(s));
+            
+            Subscription.Add(new SqLite.SubscriptionClass
+            {
+                Id = 1,
+                Amount = 5,
+                Duration = 3,
+                LieuFk = 1,
+                PaymentTypeFk = 1,
+                Raison = "Test débile qui va disparaitre",
+                Recurrence = 1
+            });
+            Subscription.Add(new SqLite.SubscriptionClass
+            {
+                Id = 2,
+                Amount = 5,
+                Duration = 3,
+                LieuFk = 1,
+                PaymentTypeFk = 1,
+                Raison = "Test débile qui va disparaitre",
+                Recurrence = 1
+            });
+            
+            Subscription.CollectionChanged += SubscriptionOnCollectionChanged;
+        }
+        catch (Exception e)
+        {
+            DisplayAlert("e", e.Message, "a");
+        }
+
+    }
+
+    private void SubscriptionOnCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+    {
+        Console.WriteLine("hey");
     }
 }
